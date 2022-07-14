420b9d8af99a4869339790d45b71cba9638af45d
peterclermy <peterclermy232@gmail.com>
2022-07-15 01:17:05 +0300

    count plus 2

--- .history/src/components/Counter_20220715011355.js
+++ .history/src/components/Counter_20220715011355.js
@@ -4,7 +4,7 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 2);
+    setCount(count + 1);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
